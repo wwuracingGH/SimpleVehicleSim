670d0a1737f140ac3f8fc114f1237b676c72ecfd
Nicole Swierstra <swiersw@wwu.edu>
2024-12-12 16:19:17 -0800

    the funny

--- simulate.py
+++ simulate.py
@@ -1,10 +1,255 @@
 from track import trackDef, trackFromBezierCSV, normalize
-from vehicle import vehicle
+from vehicle import vehicle, polynomial
 
 import matplotlib.pyplot as plt
 import matplotlib
 
 import math
+from time import sleep
+
+#heat
+colors = [
+    0,
+    88, #red, grey (coolish)
+    196, #red
+    208, #orange
+    195, #blueish
+    225, #purpleish
+    244, #grey
+    238, #grey
+]
+
+frames = [
+    ([
+        '          ',
+        '          ',
+        '    ▓▓    ',
+        '          ',
+        '          '
+    ],
+    [
+        '          ',
+        '          ',
+        '    55    ',
+        '          ',
+        '          '     
+    ], 100),
+    ([
+        '          ',
+        '    ▄▄    ',
+        '   █▓▓█   ',
+        '    ▀▀    ',
+        '          '
+    ],
+    [
+        '          ',
+        '    44    ',
+        '   4444   ',
+        '    44    ',
+        '          '     
+    ], 60),
+    ([
+        '          ',
+        '   ▄▀▀▄   ',
+        '  █ ▓▓ █  ',
+        '   ▀▄▄▀   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   5555   ',
+        '  553355  ',
+        '   5555   ',
+        '          '     
+    ], 60),
+    ([
+        '    ▄▄    ',
+        '  ▄▀  ▀▄  ',
+        ' █  ▓▓  █ ',
+        '  ▀▄  ▄▀  ',
+        '    ▀▀    '
+    ],
+    [
+        '    66    ',
+        '  66  66  ',
+        ' 6  33  6 ',
+        '  66  66  ',
+        '    66    '     
+    ], 60),
+    ([
+        '   ▄▀▀▄   ',
+        ' ▄▀ ▄▄ ▀▄ ',
+        '█  █▓▓█  █',
+        ' ▀▄ ▀▀ ▄▀ ',
+        '   ▀▄▄▀   '
+    ],
+    [
+        '   7777   ',
+        ' 77 33 77 ',
+        '7  3223  7',
+        ' 77 33 77 ',
+        '   7777   '
+    ], 60),
+    ([
+        '          ',
+        '    ▄▄    ',
+        '   █▓▓█   ',
+        '    ▀▀    ',
+        '          '
+    ],
+    [
+        '          ',
+        '    33    ',
+        '   3223   ',
+        '    33    ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '    ▓▓    ',
+        '  ▓▓▓▓▓▓  ',
+        '    ▓▓    ',
+        '          '
+    ],
+    [
+        '          ',
+        '    22    ',
+        '  221122  ',
+        '    22    ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   2111   ',
+        '  112211  ',
+        '   1211   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   1122   ',
+        '  111111  ',
+        '   1211   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   1112   ',
+        '  710711  ',
+        '   1111   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   1661   ',
+        '  166661  ',
+        '   1661   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   0000   ',
+        '  000000  ',
+        '   0000   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   0000   ',
+        '  007700  ',
+        '   0000   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '   0      ',
+        '  0 77 0  ',
+        '     00   ',
+        '          '
+    ], 60),
+    ([
+        '          ',
+        '   ▓▓▓▓   ',
+        '  ▓▓▓▓▓▓  ',
+        '   ▓▓▓▓   ',
+        '          '
+    ],
+    [
+        '          ',
+        '          ',
+        '    77    ',
+        '          ',
+        '          '
+    ], 60),
+]
+
+def explode(offset):
+    offset_spaces = ' ' * offset
+
+    for lines, cols, frametime in frames:
+        print("\033[4F")
+        computed_lines = [offset_spaces, offset_spaces, offset_spaces, offset_spaces, offset_spaces]
+        for y in range(0, len(lines)):
+            for x in range(0, len(lines[y])): 
+                if cols[y][x] != ' ':
+                    computed_lines[y] += f"\033[38;5;{colors[int(cols[y][x])]}m"+ lines[y][x]
+                else:
+                    computed_lines[y] += " "
+            print(computed_lines[y])
+        
+        sleep(frametime / 1000.0)
+        print("\033[3F")
+
+    print("\033[0m")
 
 class Simulation:
     def change_in_s(u, v, xi, curve, n):
@@ -68,14 +313,58 @@ class Simulation:
 
         self.velocity      = (0, 0) # u, v in the paper (forward, right)
         self.vehicleyawrate     = 0 # ω (omega) in the paper, dψ
+    
+    @staticmethod
+    def run_accel(vehicledata : vehicle, length : float):
+        time = 0
+        dt = 0.0166
+        velocity = 0.01
+        distance = 0
+
+        display_travel_length = 100
+        space_per_m = float(display_travel_length) / length
+        spaces = 0
+
+        print("\n")
+ 
+        print(" /| __" + " " * 94 + "|")
+        print("⌾════⌾" + " " * 94 + "|")       
+        while distance < length:
+            time += dt
+            velocity += vehicledata.max_accel_g(velocity, 70) * 9.81 * dt
+            distance += velocity * dt
+
+
+
+            if spaces < int(space_per_m * distance):
+                spaces = int(space_per_m * distance)
+
+                print("\033[2F")
+                print("\033[2F")
+                print("\033[2F")
+                print("\033[2F")
+                
+                toprint = " " * spaces
+                
+                print(vehicledata.max_accel_g(velocity, 70))
+                print(str(float(int(time * 1000)) / 1000) + ": " + str(float(int(velocity * 100)) / 100) + "m/s")
+
+                print(toprint + " /| __")
+                print(toprint + "⌾════⌾")
+            
+            sleep(dt)
+
         
+        print("YOU COMPLETED ACCEL IN " + str(float(int(time * 1000)) / 1000) + " SECONDS")
+        print(str(float(int(velocity * 1000)) / 1000)) 
 
 if __name__ == '__main__':
-    track = trackFromBezierCSV("defaulttrack.csv", 1.5)
+    car = vehicle(280, 2, 1.5, polynomial([-0.0001, 1.5]), polynomial([1.25]), 0.22,
+                polynomial([-0.125, 0.50]), polynomial([0.5]), 
+                3.3, polynomial([0.94]), 0.98, 
+                0.0, 0.0, 
+                230, polynomial([80000]), 6.2, False#polynomial([7.01587e-7, 0, 0.0793916, -19.2291, 1525.91552, 0]), 6.2
+                )
+    
+    Simulation.run_accel(car, 75)
 
-    px, py, c = track.getCurvePyplot(50000)
-
-    x = range(len(c))
-
-    plt.plot(x,c)
-    plt.show()
--- vehicle.py
+++ vehicle.py
@@ -6,94 +6,145 @@ import matplotlib.pyplot as plt
 import math, matplotlib
 AIRDENSITY = 1.204
 
+class polynomial:
+    def __init__(this, values : list[float]):
+        this.values = values
+        this.max_power = len(values)
+    
+    def f(this, x):
+        n = this.values[-1]
+        for i in range(1, this.max_power):
+            n += this.values[- (i + 1)] * x ** i
+        return n
+
+
 class vehicle:
     def __init__(
             this,
-            max_power           : float,
-            max_torque          : float,
+            # Basic
             mass                : float,
-            coeff_fric_long     : float,
-            coeff_fric_lat      : float,
+            wheelbase           : float,
+            trackwidth          : float,
+            coeff_fric_lon      : polynomial, # coeff per load N
+            coeff_fric_lat      : polynomial, # coeff per load N
             wheel_radius        : float,
+            # Suspension
+            lon_load_transfer   : polynomial, # part of mass to front per accel
+            lat_load_transfer   : polynomial, # part of mass to right side per accel
+            # Drivetrain
             final_drive_ratio   : float,
-            coeff_drag          : float,
-            frontal_area        : float,
-            coeff_downforce     : float
+            motor_efficiency    : polynomial, # part of motor efficiency per torque
+            drive_efficiency    : float,
+            # Aero
+            drag_area           : float,
+            downforce_area      : float,
+            # High Voltage
+            max_torque          : float,
+            max_power_per_soc   : polynomial,
+            capacity            : float,
+            # Misc
+            AWD                 : bool = False
             ):
-        this.max_power = max_power
-        this.max_torque = max_torque
-        this.wheel_radius = wheel_radius
-        this.final_drive_ratio = final_drive_ratio
-
         this.mass = mass
-
+        this.wheelbase = wheelbase
+        this.trackwidth = trackwidth
+        this.coeff_fric_lon = coeff_fric_lon
         this.coeff_fric_lat = coeff_fric_lat
-        this.coeff_fric_long = coeff_fric_long
+        this.wheel_radius = wheel_radius
 
-        this.coeff_drag = coeff_drag
-        this.frontal_area = frontal_area
-        this.coeff_downforce = coeff_downforce
+        this.lon_load_transfer = lon_load_transfer
+        this.lat_load_transfer = lat_load_transfer
 
-        this.tire_circumference = wheel_radius * 2 * np.radians(360)
+        this.final_drive_ratio = final_drive_ratio
+        this.motor_efficiency = motor_efficiency
+        this.drive_efficiency = drive_efficiency
+
+        this.drag_area = drag_area
+        this.downforce_area = downforce_area
+
+        this.max_torque = max_torque
+        this.max_power_per_soc = max_power_per_soc
+        this.capacity = capacity
+
+        this.tire_circumference = wheel_radius * np.radians(360)
+
+        this.AWD = AWD
 
         # END
 
-    def max_accel_from_power(this, velocity : float):
-        wheel_rps = velocity / this.tire_circumference
-        motor_rps = wheel_rps * this.final_drive_ratio
+    
+
+    def max_accel_g(this, velocity : float, soc : float):
+        motor_rps = (velocity / this.tire_circumference) * this.final_drive_ratio
+        if motor_rps * 60 > 6500: return 0
         
-        max_motor_torque = min(230, (this.max_power / (6.2831853 * motor_rps)))
-        max_wheel_force = max_motor_torque * this.final_drive_ratio / this.wheel_radius
+        d_force = this.get_downforce(velocity) + (this.mass * 9.81)
 
-        print(motor_rps)
-        if (motor_rps > 108): return 0 #if the motor rpm is greater than 6500
-        else: return  max_wheel_force / this.mass / 9.81
+        max_motor_torque = min(this.max_torque, this.max_power_per_soc.f(soc) * 9.54929677 / (motor_rps * 60))
+        max_motor_force = max_motor_torque * this.final_drive_ratio / this.wheel_radius
+        max_motor_force *= this.drive_efficiency
+        max_motor_force -= this.get_drag(velocity)
+        max_motor_g = max_motor_force / (this.mass * 9.81)
+       
+        max_tire_g = 0
 
-    def downforce(this, velocity):
-        return (this.mass + (this.coeff_downforce * (velocity**2)))
+        i = 0
+        while(max_tire_g < max_motor_g and i < 5):
+            lon_pos = this.lon_load_transfer.f(max_tire_g)
+            lon_neg = 1 - this.lon_load_transfer.f(max_tire_g)
+            lat_pos = this.lat_load_transfer.f(0)
+            lat_neg = 1 - this.lat_load_transfer.f(0)
 
-    #def tireForces(this)
-    #    def lerp(a,b,t): (1-t) * a + t * b
-    #    def invlerp(a,b,t): (t - a) / (b - a)
+            normal_force_rr = lon_neg * lat_pos * d_force
+            normal_force_lr = lon_neg * lat_neg * d_force
+            normal_force_rf = lon_pos * lat_pos * d_force
+            normal_force_lf = lon_pos * lat_neg * d_force
 
-    #    slipx = -(1 + Rww/uw) #or, k is equal to -1 - speed of the tread over the surface / velocity of the wheel center
-    #    slipa = atan(-vw/uw)  #or, a is equal to the angle between the wheels's velocity and the heading of the car
+            max_tire_force = this.coeff_fric_lon.f(normal_force_rr) * normal_force_rr
+            max_tire_force += this.coeff_fric_lon.f(normal_force_lr) * normal_force_lr
+            if this.AWD:
+                max_tire_force += this.coeff_fric_lon.f(normal_force_rf) * normal_force_rf
+                max_tire_force += this.coeff_fric_lon.f(normal_force_lf) * normal_force_lf
+            max_tire_g = max_tire_force / (this.mass * 9.81)
+            
+            i += 1
 
-    #    coeff_fxmax = lerp(cf_x1, cf_x2, invlerp(Fz1, Fz2, Fz))
-    #    coeff_fymax = lerp(cf_y1, cf_y2, invlerp(Fz1, Fz2, Fz))
-    #    slipxmax    = lerp(cf_slipx1, cf_slipx2, invlerp(Fz1, Fz2, Fz))
-    #    slipamax    = lerp(cf_slipa1, cf_slipa2, invlerp(Fz1, Fz2, Fz))
+        return min(max_motor_g, max_tire_g)
+         
+    def get_drag(this, velocity):
+        return (this.downforce_area * (velocity**2))
 
-    #    slipxn = slipx/slipmax
-    #    slipan = slipa/slipamax
+    def get_tire_f(this, normal_force):
+        return this.coeff_fric_lon  
 
-    #    p = sqrt(slipxn**2 + slipan**2)
-
-    #    ux = slipxmax*sin(Qx * arctan(pi*p/2 * arctan(Qx)))
-    #    uv = slipymax*sin(Qy * arctan(pi*p/2 * arctan(Qy)))
-
-    #    Fx = ux * Fz * slipxn/p
-    #    Fy = uy * Fz * slipan/p
-
+    def get_downforce(this, velocity):
+        return (this.downforce_area * (velocity**2))
 
     def plot_elipse(this, velocity : float):
         t = np.linspace(0, 360, 360)
         
-        x = this.coeff_fric_lat *  np.cos(np.radians(t)) * this.downforce(velocity) / this.mass
-        y = this.coeff_fric_long * np.sin(np.radians(t)) * this.downforce(velocity) / this.mass 
+        df_mult = ((this.mass * 9.81) + this.get_downforce(velocity)) / (this.mass * 9.81)
 
-        mv = this.max_accel_from_power(velocity)
+        x = this.coeff_fric_lat.f(df_mult / 2) * df_mult * np.cos(np.radians(t))
+        y = this.coeff_fric_lon.f(df_mult / 2) * df_mult * np.sin(np.radians(t))
+
+        mv = this.max_accel_g(velocity, 100)
 
         y = np.array(list(map((lambda n : min(n, mv)), y)))
 
         plt.plot(x,y, color=matplotlib.colors.hsv_to_rgb(((velocity / 60.0), 1, 1)))
 
 if __name__ == '__main__':
-    v = vehicle(80000, 230, 290, 1.4, 1.1, 0.22, 5, 0.35, 0.3, 0.3)
+    v = vehicle(280, 2, 1.5, polynomial([-0.0001, 1.7]), polynomial([1.25]), 0.22,
+                polynomial([0.5]), polynomial([0.5]), 
+                4.0, polynomial([0.94]), 0.98, 
+                0.5, 0.5, 
+                300, polynomial([80000]), 6.2, #[7.01587e-7, 0, 0.0793916, -19.2291, 1525.91552, 0]), 6.2
+                )
     
     plt.gca().set_aspect('equal')
-    for i in range(0, 6):
-        v.plot_elipse(float(i * 10))
+    for i in range(0, 9):
+        v.plot_elipse(float(i * 5) + 2.43) #7312455278365)
 
     plt.title("Max accleration at speed")
     plt.show()
